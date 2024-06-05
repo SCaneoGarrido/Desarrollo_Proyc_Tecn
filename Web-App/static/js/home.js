4792c68f1268baf0695f74cdbccfbd7ef83e826a
B3njamin-f <fierro912@gmail.com>
2024-06-05 00:48:29 -0400

    home restaurado, no se habia subido el js correspondiente

---
+++ Web-App/static/js/home.js
@@ -0,0 +1,27 @@
+// Función para ocultar todas las secciones
+function hideAllSections() {
+    document.getElementById('inicio').style.display = 'none';
+    document.getElementById('tusCargas').style.display = 'none';
+    document.getElementById('soporte').style.display = 'none';
+    document.getElementById('cuenta').style.display = 'none';
+}
+
+// Función para mostrar la sección seleccionada
+function showSection(sectionId) {
+    hideAllSections();
+    document.getElementById(sectionId).style.display = 'block';
+}
+
+// Eventos de clic para los enlaces de la barra de navegación
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.menu-group .item').forEach(item => {
+        item.addEventListener('click', function(e) {
+            e.preventDefault(); // Previene el comportamiento por defecto de los enlaces
+            const sectionId = this.getAttribute('href').substring(1); // Obtiene el ID de la sección desde el href del enlace
+            showSection(sectionId);
+        });
+    });
+
+    // Mostrar la sección de landing page por defecto al cargar la página
+    showSection('inicio');
+});
