2dfde3a6c43615053c1a056162733c3ad5ba49b3
SCaneoGarrido <sebamc403@gmail.com>
2024-06-06 10:35:14 -0400

    rama dev

--- Web-App/api.py
+++ Web-App/api.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, send_file, session, redirect, url_for, render_template # type: ignore
 from flask_cors import CORS # type: ignore
 from datetime import datetime, timedelta
-from flask_session import Session
+from flask_session import Session # type: ignore
 from Utilidades.manage_credential import credentialsUser
 from Utilidades.manejo_db import db_manage
 from Utilidades.autenticacion import autenticacion
